c37078abed68058b57168bc29ae9d0dba2bbeefa
MehdiZaidaneS <mehdicontacto@gmail.com>
2022-01-18 10:36:14 +0100

    SomeExampleComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // App.tsx
 import React from 'react'
+import SomeExampleComponent from './components/example/someExampleComponent';
 import MyFirstComponent from './components/myFirstComponent';
 
 const App: React.FC = () => {
   return (
     <div>
       <MyFirstComponent />
+      <SomeExampleComponent />
     </div>
   )
 }
