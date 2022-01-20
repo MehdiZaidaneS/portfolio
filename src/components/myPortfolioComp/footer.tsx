3658b25632f57514df3d82a8b8a18dcc33303486
MehdiZaidaneS <mehdicontacto@gmail.com>
2022-01-20 14:08:50 +0100

    Changes 2x

--- src/components/myPortfolioComp/footer.tsx
+++ src/components/myPortfolioComp/footer.tsx
@@ -16,17 +16,17 @@ const Footer: React.FC<FooterProps> = () => {
     <div>
       <div className={styles.socialMedia}>
           <div className={styles.twitter}>
-            <a href="https://twitter.com/BarackObama" target={'_blank'}>
+            <a href="https://twitter.com/BarackObama" target={'_blank'} rel='noreferrer' >
             <FontAwesomeIcon icon={faTwitter} size="2x" />
             </a>
           </div>
           <div className={styles.instagram}>
-            <a href="https://www.instagram.com/mehdi_zdn5" target={'_blank'}>
+            <a href="https://www.instagram.com/mehdi_zdn5" target={'_blank'} rel='noreferrer'>
             <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
           </div>
           <div className={styles.facebook}>
-            <a href="https://www.facebook.com/mehdi.zaidane.56/" target={'_blank'}>
+            <a href="https://www.facebook.com/mehdi.zaidane.56/" target={'_blank'} rel='noreferrer'>
             <FontAwesomeIcon icon={faFacebook} size="2x" />
             </a>
           </div>
