3dca0a7f6136650511d8218d5362d0fa0b0fdeb2
MehdiZaidaneS <mehdicontacto@gmail.com>
2022-02-01 13:55:48 +0100

    Navbars

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import Etusivu from './components/myPortfolioComp/etusivu';
 import styles from './App.module.scss';
 import Footer from './components/myPortfolioComp/footer';
 import Projektisivu from './components/myPortfolioComp/projektisivu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const App: React.FC = () => {
 
@@ -20,13 +22,34 @@ const App: React.FC = () => {
     <Router basename='/portfolio'>
       
       {/* The navigation bar and other components you want to display on all pages come here */}
-    
-      <div className={styles.barra}>
-        { visible === true &&
+      
+
+      <div className={visible === true ? styles.barra : styles.barra2}>
+        {
+         visible === true &&
         <div className={styles.navbar}>
+        <h1>Menu</h1>
         <Link to='/'>-Home</Link>
         <Link to='projects'>-Projects</Link>
         <Link to= 'cv'>-CV</Link>
+        <div className={styles.socialMediaa}>
+          <div className={styles.twitter}>
+            <a href="https://twitter.com/BarackObama" target={'_blank'} rel='noreferrer' >
+            <FontAwesomeIcon icon={faTwitter} size="1x" />
+            </a>
+          </div>
+          <div className={styles.instagram}>
+            <a href="https://www.instagram.com/mehdi_zdn5" target={'_blank'} rel='noreferrer'>
+            <FontAwesomeIcon icon={faInstagram} size="1x" />
+            </a>
+          </div>
+          <div className={styles.facebook}>
+            <a href="https://www.facebook.com/mehdi.zaidane.56/" target={'_blank'} rel='noreferrer'>
+            <FontAwesomeIcon icon={faFacebook} size="1x" />
+            </a>
+          </div>
+          
+      </div>
         </div>
         }
         <div className={styles.abrir}>
@@ -43,7 +66,9 @@ const App: React.FC = () => {
   
 
       {/* The footer and other components you want to display on all pages come here */}
+     
       <Footer />
+      
 
     </Router>
   )
